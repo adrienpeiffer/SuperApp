e94791cf27865f34e85d62d327f539ec88159507
Adrien PEIFFER <Adrien.PEIFFER@ainos.lu>
2020-10-20 18:44:35 +0200

    Change message

--- SuperApp/Program.cs
+++ SuperApp/Program.cs
@@ -6,7 +6,7 @@ namespace SuperApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Elgon!");
         }
     }
 }
